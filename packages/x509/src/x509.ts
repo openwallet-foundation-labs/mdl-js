3ee9bfa52311a4794549e767324330690132678c
Lukas <Lukas@hopae.io>
2024-06-03 10:27:43 +0900

    feat: add parsed files

--- packages/x509/src/x509.ts
+++ packages/x509/src/x509.ts
@@ -1,132 +1,6 @@
-const oidMap: { [key: string]: string } = {
-  '2a864886f70d010101': 'rsaEncryption',
-  '2a864886f70d01010b': 'sha256WithRSAEncryption',
-  '2a864886f70d01010c': 'sha384WithRSAEncryption',
-  '2a864886f70d01010d': 'sha512WithRSAEncryption',
-  '2a8648ce3d0201': 'ecPublicKey',
-  '2a8648ce3d040301': 'ecdsa-with-SHA256',
-  '2a8648ce3d040302': 'ecdsa-with-SHA384',
-  '2a8648ce3d040303': 'ecdsa-with-SHA512',
-  '608648016503040201': 'sha256',
-  '608648016503040202': 'sha384',
-  '608648016503040203': 'sha512',
-  '2a864886f70d010701': 'data',
-  '2a864886f70d010702': 'signedData',
-  '2a864886f70d010901': 'emailAddress',
-  '550403': 'commonName',
-  '550404': 'surname',
-  '550405': 'serialNumber',
-  '550406': 'countryName',
-  '550407': 'localityName',
-  '550408': 'stateOrProvinceName',
-  '550409': 'streetAddress',
-  '55040a': 'organizationName',
-  '55040b': 'organizationalUnitName',
-  '55040c': 'title',
-  '55040d': 'description',
-  '55042a': 'givenName',
-  '551d0e': 'subjectKeyIdentifier',
-  '551d0f': 'keyUsage',
-  '551d11': 'subjectAltName',
-  '551d13': 'basicConstraints',
-  '551d20': 'certificatePolicies',
-  '551d23': 'authorityKeyIdentifier',
-  '551d12': 'issuerAltName',
-  '551d1f': 'cRLDistributionPoints',
-};
+import { oidMap } from './oid';
 
-class ASN1Parser {
-  private buffer: ArrayBuffer;
-  private view: DataView;
-  private offset: number;
-
-  constructor(buffer: ArrayBuffer) {
-    this.buffer = buffer;
-    this.view = new DataView(this.buffer);
-    this.offset = 0;
-  }
-
-  private parseTag(): number {
-    return this.view.getUint8(this.offset++);
-  }
-
-  private parseLength(): number {
-    let length = this.view.getUint8(this.offset++);
-    if (length & 0x80) {
-      const numberOfBytes = length & 0x7f;
-      length = 0;
-      for (let i = 0; i < numberOfBytes; i++) {
-        length = (length << 8) | this.view.getUint8(this.offset++);
-      }
-    }
-    return length;
-  }
-
-  private parseValue(tag: number, length: number): any {
-    if ((tag & 0x20) === 0x20) {
-      // Constructed types
-      const endOffset = this.offset + length;
-      const value: any[] = [];
-      while (this.offset < endOffset) {
-        value.push(this.parse());
-      }
-      return value;
-    } else {
-      // Primitive types
-      const value = this.buffer.slice(this.offset, this.offset + length);
-      this.offset += length;
-
-      // Check if the value is a sequence
-      if (tag === 0x04 && new DataView(value).getUint8(0) === 0x30) {
-        const sequenceParser = new ASN1Parser(value);
-        return sequenceParser.parse().value;
-      }
-
-      return value;
-    }
-  }
-
-  public parse(): any {
-    const tag = this.parseTag();
-    const length = this.parseLength();
-    const value = this.parseValue(tag, length);
-
-    return {
-      tag,
-      length,
-      value,
-    };
-  }
-
-  public printParsedData(parsedData: any, indent: string = ''): void {
-    const { tag, length, value } = parsedData;
-    console.log(
-      `${indent}Tag: ${tag.toString(16).padStart(2, '0').toUpperCase()}`,
-    );
-    console.log(`${indent}Length: ${length}`);
-    if (Array.isArray(value)) {
-      console.log(`${indent}Value: [`);
-      value.forEach((item: any) => this.printParsedData(item, indent + '  '));
-      console.log(`${indent}]`);
-    } else {
-      console.log(`${indent}Value: ${this.formatValue(value)}`);
-    }
-  }
-
-  private formatValue(value: any): string {
-    if (value instanceof ArrayBuffer) {
-      return Array.from(new Uint8Array(value))
-        .map((byte) => byte.toString(16).padStart(2, '0'))
-        .join(' ');
-    } else if (Array.isArray(value)) {
-      return JSON.stringify(value);
-    } else {
-      return String(value);
-    }
-  }
-}
-
-class X509Certificate {
+export class X509Certificate {
   private parsedData: any;
 
   constructor(parsedData: any) {
@@ -195,16 +69,16 @@ class X509Certificate {
     return rdnSequence
       .map((rdnSet: any) => {
         const rdn = rdnSet.value[0];
-        const oid = this.oidToName(rdn.value[0].value);
+        const oid = this.oidToSimpleName(rdn.value[0].value);
         const value = this.bufferToString(rdn.value[1].value);
         return `${oid}=${value}`;
       })
       .join(', ');
   }
 
-  private oidToName(oid: ArrayBuffer): string {
+  private oidToSimpleName(oid: ArrayBuffer): string {
     const oidHex = this.toHexString(oid);
-    const oidMap: { [key: string]: string } = {
+    const simpleOidMap: { [key: string]: string } = {
       '550406': 'C', // Country Name
       '550408': 'ST', // State or Province Name
       '55040a': 'O', // Organization Name
@@ -212,7 +86,7 @@ class X509Certificate {
       '550403': 'CN', // Common Name
       '550409': 'L', // Locality Name
     };
-    return oidMap[oidHex] || `Unknown OID: ${oidHex}`;
+    return simpleOidMap[oidHex] || `Unknown OID: ${oidHex}`;
   }
 
   private bufferToString(buffer: ArrayBuffer): string {
@@ -368,35 +242,3 @@ class X509Certificate {
     return usage;
   }
 }
-
-// 사용 예제
-const pem = `MIIBXjCCAQSgAwIBAgIGAY/YZ7BdMAoGCCqGSM49BAMCMDYxNDAyBgNVBAMMKzJJ
-NjZKNi1lbGx4N2pIY3RnRVMyZEdXbGhoRGhzSXZ1X0dBU1B1Q0VGMDgwHhcNMjQw
-NjAyMTAwMzQ1WhcNMjUwMzI5MTAwMzQ1WjA2MTQwMgYDVQQDDCsySTY2SjYtZWxs
-eDdqSGN0Z0VTMmRHV2xoaERoc0l2dV9HQVNQdUNFRjA4MFkwEwYHKoZIzj0CAQYI
-KoZIzj0DAQcDQgAEWn/dSPASseG60dh2H4Hsqy2zGC8lDQstyrXo2GsKmHiTbv/y
-Se5TikdKulSRWGF4qERk+YUQN8LnJBC+rnN15DAKBggqhkjOPQQDAgNIADBFAiEA
-3Y8VJnZJV6QwBZP+Q9Bq/76FJG+yqyS/kdsQgYhBfwACIEyctJoEVUAbLxw1uVhk
-IKfehXmTBmqzb064GBeqJ++P`;
-
-const binaryCert = Uint8Array.from(atob(pem), (c) => c.charCodeAt(0)).buffer;
-
-const parser = new ASN1Parser(binaryCert);
-const parsedCert = parser.parse();
-//parser.printParsedData(parsedCert);
-const cert = new X509Certificate(parsedCert);
-console.log(`Version: ${cert.getVersion()}`); // 예상 출력: Version: 3
-console.log(`Serial Number: ${cert.getSerialNumber()}`); // 예상 출력: Serial Number: 5ddd2890e38ce5cca517073658d2bb0f63be02b0
-console.log(`Signature Algorithm: ${cert.getSignatureAlgorithm()}`); // 예상 출력: Signature Algorithm: ecdsa-with-SHA256
-console.log(`Issuer: ${cert.getIssuer()}`); // 예상 출력: Issuer: C=US, ST=US-CA, O=CA-DMV, CN=California DMV IACA Root
-const validity = cert.getValidity();
-console.log(
-  `Validity: Not Before: ${validity.notBefore}, Not After: ${validity.notAfter}`,
-); // 예상 출력: 유효기간 정보
-console.log(`Subject: ${cert.getSubject()}`); // 예상 출력: Subject: C=US, ST=US-CA, O=CA-DMV, CN=California DMV IACA Root
-const subjectPublicKeyInfo = cert.getSubjectPublicKeyInfo();
-console.log(
-  `Subject Public Key Info: Algorithm: ${subjectPublicKeyInfo.algorithm}, Public Key: ${subjectPublicKeyInfo.publicKey}`,
-); // 예상 출력: 공개 키 정보
-const extensions = cert.getExtensions();
-console.log('Extensions:', extensions); // 예상 출력: 확장 필드 정보
